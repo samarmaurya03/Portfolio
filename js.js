4e4af99d5d4b0f3e8829fafce99f02ce2a00e1b7
Samar Maurya <118624913+samarmaurya03@users.noreply.github.com>
2025-07-03 20:30:05 +0530

    Add files via upload

---
+++ js.js
@@ -0,0 +1,14 @@
+let mob = document.getElementById("mob-bar")
+let flag = true
+let st = document.getElementById("st-right")
+let stleft = document.getElementById("st-left")
+let back = document.getElementById("back")
+function display(){
+        back.style.display = "block"
+        flag = false
+    
+
+}
+function none(){
+    back.style.display = "none"
+}
